b446af3ad5555441f7bc6ee60b0a70ad34e8a899
sanc0o0 <sanansari0305@gmail.com>
2025-10-12 00:58:54 +0530

    Update: Make the get started button interctive

--- js/script.js
+++ js/script.js
@@ -5,3 +5,8 @@ document.querySelector(".login-btn").addEventListener("click", function () {
 document.querySelector(".signup-btn").addEventListener("click", function () {
   window.location.href = "../Nav files/signup.html"; // sign up page open karega
 });
+
+document.querySelector(".get-started-btn").addEventListener("click", function () {
+  window.location.href = "Nav files/login.html"; // sign up page open karega
+});
+  
