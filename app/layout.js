b516a765981f449104bb376c5cdc03ad2df3c239
sanc0o0 <sanansari0305@gmail.com>
2026-04-05 07:29:45 +0530

    chore: Remove analytics from layout

--- app/layout.js
+++ app/layout.js
@@ -20,7 +20,7 @@ export default function RootLayout({ children }) {
         <main className="grow">{children}</main>
 
         <Footer />
-        <Analytics />
+        
       </body>
     </html>
   );
